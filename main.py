74b4194d4bb28e0f5663f5fcab8a08cbfffcf4ee
Saket Pawar <Spawar43@gatech.edu>
2024-02-10 08:36:38 -0500

    added to env, added main file, working on camera stream

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+import cv2
+import dash
+import pandas as pd
+import numpy as np
+from dash import dcc
+from dash.dependencies import Input, Output
+from dash import html
+
+
+# stream webcam and show in frame
+
+get_frame()
